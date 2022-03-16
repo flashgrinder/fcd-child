f73be0fc649f81c8f1cef53c56fda4ec85b240ee
flashgrinder <flashgrinder92@gmail.com>
2022-03-16 16:16:51 +0400

    start project

--- smartgrid.js
+++ smartgrid.js
@@ -2,9 +2,9 @@ module.exports = {
 	filename: "_smart-grid",
 	outputStyle: 'scss',
 	columns: 12,
-	offset: "30px",
+	offset: "20px",
 	container: {
-		maxWidth: "1260px",
+		maxWidth: "1520px",
 		fields: "30px"
 	},
 	breakPoints: {
