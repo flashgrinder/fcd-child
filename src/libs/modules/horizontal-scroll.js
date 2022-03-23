6315becd2d166ad572aa918cad4167e8f9d44a96
flashgrinder <flashgrinder92@gmail.com>
2022-03-23 14:33:04 +0400

    test scroll trigger

--- src/libs/modules/horizontal-scroll.js
+++ src/libs/modules/horizontal-scroll.js
@@ -7,7 +7,34 @@ function init() {
 
     let sections = gsap.utils.toArray(".screen");
     let container = document.querySelector(".horizontal-container");
+    
+    // gsap.to(container, {
+    //     x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+    //     ease: "none",
+    //     scrollTrigger: {
+    //       trigger: container,
+    //       invalidateOnRefresh: true,
+    //       markers: true,
+    //       pin: true,
+    //       scrub: 1,
+    //       snap: 1 / (sections.length - 1),
+    //       end: () => "+=2500" + sections.offsetWidth
+    //     }
+    // })
 
+    gsap.to(container, {
+        x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+        ease: "none",
+        scrollTrigger: {
+          trigger: container,
+          invalidateOnRefresh: true,
+          markers: true,
+          pin: true,
+          scrub: 1,
+        //   snap: 1 / (sections.length - 1),
+          end: () => "+=2500"
+        }
+    })
     // gsap.to(sections, {
     //     xPercent: -100 * (sections.length - 1),
     //     ease: "none",
@@ -21,19 +48,39 @@ function init() {
     //     }
     // });
 
-    gsap.to(container, {
-        x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+    // const tl = gsap.timeline({
+    //     scrollTrigger: {
+    //         trigger:".screen-entry",
+    //         scrub: true,
+    //         pin:true,
+    //         start:"top top",
+    //         end:"+=100%"
+    //     }
+    // });
+
+    // tl.from('.screen-entry__door', 3, {
+    //     x: '40px',
+    //     // autoAlpha: 0
+    // }, '+=1');
+
+
+    const tl = gsap.timeline( {
+        x: 0,
         ease: "none",
         scrollTrigger: {
-          trigger: container,
-          invalidateOnRefresh: true,
-          pin: true,
-          scrub: 1,
-          snap: 1 / (sections.length - 1),
-          end: () => "+=2500" + sections.offsetWidth
+            trigger: ".screen",
+            start: "top center",
+            // end: () => "+=2500" + sections.offsetWidth,
+            end:"+=100%",
+            scrub: 1,
+            // snap: 1 / (sections.length - 1),
         }
-      })
+    });
 
+    tl.from(".screen-entry__door", 1, {
+        x: 1000,
+        
+    }, '-=1')
 
 }
 
