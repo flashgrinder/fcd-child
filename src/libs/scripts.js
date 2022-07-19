4ca88f93fa5b65330a91f71d7ad3b793eb8781b8
flashgrinder <flashgrinder92@gmail.com>
2022-07-19 15:23:54 +0400

    make tarif slider

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import horizontalScroll from '../libs/modules/horizontal-scroll';
 import progressBar from '../libs/modules/progress-bar';
 import parallaxjs from '../libs/modules/parallax';
+import tarifsSlider from '../libs/modules/tarifs';
 
 document.addEventListener('DOMContentLoaded', function(e) {
 
@@ -12,8 +13,10 @@ document.addEventListener('DOMContentLoaded', function(e) {
     const horizintalScrollTrue = document.querySelector('.horizontal-container');
     const progressBarTrue = document.querySelector('.progress');
     const paralaxjsTrue = document.querySelector('.parallax-container');
+    const tarifsSliderTrue = document.querySelector('.tarifs__slider');
     
     horizintalScrollTrue ? horizontalScroll.init() : false;
     progressBarTrue ? progressBar.init() : false;
     paralaxjsTrue ? parallaxjs.init() : false;
+    tarifsSliderTrue ? tarifsSlider.init() : false;
 });
