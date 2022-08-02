1f0a6e2a7d884707cdb6c27c928ae2531257ab42
flashgrinder <flashgrinder92@gmail.com>
2022-08-02 17:55:06 +0400

    make theme page 404

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+    <!-- Page-404 -->
+    <section class="page-404">
+        <div class="page-404__outer parallax-container">
+            <div class="page-404__pic ">
+                <img src="<?php echo STANDART_DIR; ?>img/404/404-bg.svg" class="page-404__img page-404__img--bg parallax-js" data-speed-x="5" data-speed-y="5" alt="">
+                <img src="<?php echo STANDART_DIR; ?>img/404/404-board.svg" class="page-404__img page-404__img--board parallax-js" data-speed-x="10" data-speed-y="7" alt="">
+                <img src="<?php echo STANDART_DIR; ?>img/404/404-frame.svg" class="page-404__img page-404__img--frame parallax-js" data-speed-x="15" data-speed-y="9" alt="">
+                <img src="<?php echo STANDART_DIR; ?>img/404/404-table.svg" class="page-404__img page-404__img--table parallax-js" data-speed-x="20" data-speed-y="11" alt="">
+            </div>
+            <div class="page-404__body container">
+                <div class="page-404__inner parallax-js" data-speed-x="25" data-speed-y="13">
+                    <img src="<?php echo STANDART_DIR; ?>img/404/404.svg" alt="" class="page-404__img-404">
+                    <h3 class="page-404__title title text--white title--w-light">
+                        Извините, страница не найдена
+                    </h3>
+                    <div class="page-404__action">
+                        <a href="<?php echo home_url(); ?>" class="page-404__btn button button--primary">
+                            На главную
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ./ Page-404 -->
+
+<?php get_footer(); ?>
