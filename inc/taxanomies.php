ee5574339388ba42e84c841eb3a323d7f7af131c
flashgrinder <flashgrinder92@gmail.com>
2022-09-28 15:13:15 +0400

    remove page tax

--- inc/taxanomies.php
+++ inc/taxanomies.php
@@ -2,34 +2,6 @@
 
     add_action('init', function(){
 
-        // Категории для страниц
-        register_taxonomy('cats_page', array('page'), array(
-            'label'                 => '', // определяется параметром $labels->name
-            'labels'                => array(
-                'name'              => 'Название Категории',
-                'singular_name'     => 'Категория',
-                'search_items'      => 'Найти Категорию',
-                'all_items'         => 'Все Категории',
-                'view_item '        => 'Смотреть Категорию',
-                'parent_item'       => 'Родитель Категории',
-                'parent_item_colon' => 'Родитель Категория:',
-                'edit_item'         => 'Редактировать Категорию',
-                'update_item'       => 'Обновить Категорию',
-                'add_new_item'      => 'Добавить новую Категорию',
-                'new_item_name'     => 'Новое имя Категории',
-                'menu_name'         => 'Категории',
-            ),
-            'show_admin_column'     => true,
-            'public'                => true,
-            'show_in_nav_menus'     => false, // равен аргументу public
-            'show_ui'               => true, // равен аргументу public
-            'show_in_menu'          => true, // равен аргументу show_ui
-            'show_in_rest'          => true,
-            'show_tagcloud'         => true, // равен аргументу show_ui
-            'hierarchical'          => true,
-            'query_var'             => true,
-        ));
-
         // Категории для кейсов
         register_taxonomy('cats_cases', array('cases'), array(
             'label'                 => '', // определяется параметром $labels->name
